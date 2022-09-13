e79b397d61ac4e5935f420872db24029a9d7974d
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-13 09:44:01 +0300

    strikethrough

--- app/src/main/java/com/example/todolist/TodoAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.todolist
 
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_todo.view.*
 
 class TodoAdapter(
     private val todos: MutableList<Todo>
@@ -20,14 +23,26 @@ class TodoAdapter(
             )
         )
     }
-
+    private fun toggleStrikeThrough(tvTodoTitle: TextView,isChecked: Boolean){
+        if (isChecked){
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+        } else {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
+    }
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         val curTodo = todos[position]
         holder.itemView.apply {
             tvTodoTitle.text = curTodo.title
             cdDone.isChecked = curTodo.isChecked
+            toggleStrikeThrough(tvTodoTitle , curTodo.isChecked)
+            cbDone.setOnCheckedChangeListener{_, isChecked ->
+                toggleStrikeThrough(tvTodoTitle, isChecked )
+                curTodo.isChecked = !curTodo.isChecked
+            }
         }
     }
+
 
     override fun getItemCount(): Int {
         return todos.size
